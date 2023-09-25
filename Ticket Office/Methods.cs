46637a0a0448c0649fd3e0ad5560494e822ba748
Jon Mendizabal <jonmendi@gmail.com>
2023-09-25 21:41:46 +0200

    Add project files.

---
+++ Ticket Office/Methods.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket_Office
+{
+    internal class Methods
+    {
+        public static int PriceSetter(int age, string place)
+        {
+            bool seated = place == "seated";
+
+            if (age <= 11)
+            {
+                switch (seated)
+                {
+                    case true: return 50;
+                    case false: return 25;
+                }
+
+            }
+            
+            else if (age >= 65)
+            {
+                switch (seated)
+                {
+                    case true: return 100;
+                    case false: return 60;
+                }
+            }
+
+            else
+            {
+                switch (seated)
+                {
+                    case true: return 170;
+                    case false: return 110;
+                }
+            }
+        }
+
+
+        public static decimal TaxCalculator(int price)
+        {
+            decimal tax = (decimal)((1 - 1 / 1.06) * price);
+            return tax; 
+        }
+
+
+        public static int TicketNumberGenerator()
+        {
+            Random random = new Random();
+            return random.Next(1, 8001);
+        }
+
+
+        public static string ToLower(string input)
+        {
+            return input.ToLower();
+        }
+
+        public static string AntiCheat()
+        {
+            bool correct = false;
+            return "Hallo"; //*****************************REMOVE*********************
+            while (!correct)
+            {
+                string userInput = Console.ReadLine();
+                string userInputLower = Methods.ToLower(userInput);
+
+                if (userInputLower == "seated")
+                {
+                    correct = true;
+                    Console.WriteLine();
+                    Console.WriteLine("Thank you for your answers!");
+
+                }
+
+                else if (userInputLower == "standing")
+                {
+                    correct = true;
+                    Console.WriteLine();
+                    Console.WriteLine("Thank you for your answers!");
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Sorry, I didn't understand. Do you want a standing or seated ticket?");
+                }
+            }
+        }
+    }
+}
---
+++ Ticket Office/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Ticket_Office;
+
+Console.WriteLine("Welcome to The Ticket Office!");
+Console.WriteLine();
+Console.WriteLine("How old are you?");
+
+int age = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine("What type of ticket would you like to purchase? Standing or seated?");
+string place = Console.ReadLine();
+string placeLower = Methods.ToLower (place);
+
+int price = Methods.PriceSetter(age, placeLower);
+decimal tax = Methods.TaxCalculator(price);
+int rng = Methods.TicketNumberGenerator();
+
+Console.WriteLine("Here are the details of your ticket");
+Console.WriteLine();
+Console.WriteLine($"Price: \t\t{price} SEK");
+Console.WriteLine($"Taxes: \t\t{tax} SEK");
+Console.WriteLine($"Ticket Number: \t{rng}");
+Console.ReadKey();
+
+
