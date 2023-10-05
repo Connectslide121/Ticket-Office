be6114296f32999ebc9fc6ea25d0e4da2729b219
Jon Mendizabal <jonmendi@gmail.com>
2023-10-05 12:07:10 +0200

    Add test for TaxTotal method

--- Unit Test Project/UnitTest1.cs
+++ Unit Test Project/UnitTest1.cs
@@ -6,6 +6,55 @@ namespace Unit_Test_Project
     public class UnitTest1
     {
 
+        [Fact]
+        public void TaxFormula()
+        {
+            for (int i = 0; i < 5000; i++)
+            {
+                Random randomAge = new Random();
+                int age = randomAge.Next(1, 106);
+
+
+                PlaceOptions.CustomerPlacePreference place;
+                Random randomPlace = new Random();
+                int randomPlaceDefiner = randomPlace.Next(1, 3);
+                if (randomPlaceDefiner == 1)
+                {
+                    place = PlaceOptions.CustomerPlacePreference.Seated;
+                }
+                else
+                {
+                    place = PlaceOptions.CustomerPlacePreference.Standing;
+                }
+
+
+                Ticket ticket = new Ticket(age, place);
+                TicketSalesManager.ticketSalesManager.tickets.Add(ticket);
+            }
+
+            decimal totalTax = 0;
+
+            foreach (Ticket item in TicketSalesManager.ticketSalesManager.tickets)
+            {
+                totalTax += Convert.ToDecimal(item.Tax());
+            }
+
+            decimal totalSales = 0;
+
+            foreach (Ticket item in TicketSalesManager.ticketSalesManager.tickets)
+            {
+                totalSales += Convert.ToDecimal(item.Price());
+            }
+
+            decimal multiplier = 1 - 1 / 1.06m;
+            decimal taxMethod = Decimal.Multiply(multiplier, totalSales);
+
+            decimal expected = totalTax;
+            decimal actual = taxMethod;
+            int precision = 5;
+            Assert.Equal(expected, actual, precision);
+        }
+
         [Fact] 
         public void ChildSeated()
         {
