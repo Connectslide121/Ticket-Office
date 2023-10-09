7a2339c8f36104c471cff563871a50fad9d9ed8c
Jon Mendizabal <jonmendi@gmail.com>
2023-10-09 14:34:02 +0200

    Add concert class and LINQ queries

--- Ticket Office/Program.cs
+++ Ticket Office/Program.cs
@@ -1,7 +1,85 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
+using System.Text.Json;
 using Ticket_Office;
+
+string concertData = File.ReadAllText("C:\\Users\\Jon\\Desktop\\Lexicon .NET\\Assignments\\Week 3 - Ticket Office\\Ticket Office\\Ticket Office\\concert_data.json");
+
+List<Concert> concerts = JsonSerializer.Deserialize<List<Concert>>(concertData);
+
+//*****************************************************************************//
+
+DateTime currentDate = DateTime.Now;
+
+List<Concert> dateSorted = concerts
+    .Where(item => item.Date >= currentDate)
+    .OrderBy(item => item.Date)
+    .ToList();
+
+string listDateSorted = "Future concerts sorted by date:\n";
+foreach (Concert concert in dateSorted)
+{
+    listDateSorted += $"{concert.Date} @{concert.BeginsAt}:00 - ID:{concert.Id}\t- Reduced venue {concert.ReducedVenue}\t- {concert.Performer}\t- {concert.FullCapacitySales}SEK\n";
+}
+Console.WriteLine(listDateSorted);
+
+//*****************************************************************************//
+
+List<Concert> reducedVenue = concerts
+    .Where(item => item.ReducedVenue == true)
+    .ToList();
+
+string listReducedVenue = "Concerts with reduced venue:\n";
+foreach (Concert concert in reducedVenue)
+{
+    listReducedVenue += $"{concert.Date} @{concert.BeginsAt}:00 - ID:{concert.Id}\t- {concert.Performer}\t- {concert.FullCapacitySales}SEK\n";
+}
+Console.WriteLine(listReducedVenue);
+
+//*****************************************************************************//
+
+List<Concert> concerts2024 = concerts
+    .Where (item => item.Date.Year == 2024)
+    .ToList ();
+
+string listConcerts2024 = "Concerts in 2024:\n";
+foreach (Concert concert in concerts2024)
+{
+    listConcerts2024 += $"{concert.Date} @{concert.BeginsAt}:00 - ID:{concert.Id}\t- Reduced venue {concert.ReducedVenue}\t- {concert.Performer}\t- {concert.FullCapacitySales}SEK\n";
+}
+Console.WriteLine (listConcerts2024);
+
+//*****************************************************************************//
+
+List<Concert> topRevenue = concerts
+    .OrderByDescending(item => item.FullCapacitySales)
+    .Take(5)
+    .ToList();
+
+string listTopRevenue = "Top 5 full capacity revenue concerts:\n";
+foreach (Concert concert in topRevenue)
+{
+    listTopRevenue += $"{concert.Date} @{concert.BeginsAt}:00 - ID:{concert.Id}\t- Reduced venue {concert.ReducedVenue}\t- {concert.Performer}\t- {concert.FullCapacitySales}SEK\n";
+}
+Console.WriteLine(listTopRevenue);
+
+//*****************************************************************************//
+
+List<Concert> onFriday = concerts
+    .Where((concert) => concert.Date.DayOfWeek == DayOfWeek.Friday)
+    .ToList();
+
+string listOnFriday = "Concerts taking place on Fridays:\n";
+foreach (Concert concert in onFriday)
+{
+    listOnFriday += $"{concert.Date} @{concert.BeginsAt}:00 - ID:{concert.Id}\t- Reduced venue {concert.ReducedVenue}\t- {concert.Performer}\t- {concert.FullCapacitySales}SEK\n";
+}
+Console.WriteLine(listOnFriday);
+
+//*****************************************************************************//
+
+Console.ReadKey();
 
 TicketSalesManager.RandomListGenerator(7500);
 
